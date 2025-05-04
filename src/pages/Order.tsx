8b25d011ae23bcf165615f48caf643b629f7db33
Varun <153203211+confusedhumor@users.noreply.github.com>
2025-05-04 19:48:08 +0530

    Update Order.tsx

--- src/pages/Order.tsx
+++ src/pages/Order.tsx
@@ -65,9 +65,8 @@ function Order() {
       }
     } catch (error) {
       console.error("Error placing the order:", error);
-      toast("✅ ✅ Thank you for your order!<br /><br />
-    We'll reach out to confirm shortly.
-  </>");
+      toast("✅ Thank you for your order!
+    We'll reach out to confirm shortly.");
     } finally {
       setIsSubmitting(false);
     }
